bacfc0b281594547ddba5c4da6b7dea8b01b8fe9
Camden Voigt <camdenvoigt@gmail.com>
2025-04-24 17:51:33 -0700

    Added serve wrapper

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,20 @@ mod handlers;
 mod quote;
 mod templates;
 
-#[tokio::main]
-async fn main() {
+async fn serve() -> Result<(), Box<dyn std::error::Error>> {
     let app = Router::new().route("/", get(handlers::handle_get_index));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
     axum::serve(listener, app).await.unwrap();
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    if let Err(err) = serve().await {
+        eprintln!("quote-server: error: {}", err);
+        std::process::exit(1);
+    }
 }
